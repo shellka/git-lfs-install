b4feabd69a1b87c881f243344b3a52a9e6ea9176
Leonid Brish <leonid.v.brish@gmail.com>
2017-03-23 16:13:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,3 @@ info 'Installing lfs'
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git lfs install
-
-info 'Updating repository'
-git pull
